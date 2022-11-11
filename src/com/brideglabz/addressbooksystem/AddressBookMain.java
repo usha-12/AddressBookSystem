bb21073714c4a9de59c8fdb90ed718e7899d6ea5
usha-12 <usha12ahirwar@gmail.com>
2022-11-11 23:03:55 +0530

    Display Welcome Message

---
+++ src/com/brideglabz/addressbooksystem/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.brideglabz.addressbooksystem;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------Welcome to the AddressBookSystem program-------------------------------------");
+    }
+}
