c1f4ada8b5ab6dba7aa655e4738184013bc56084
usha-12 <usha12ahirwar@gmail.com>
2022-11-11 23:57:51 +0530

    Ability to create a contacts in address book

--- src/com/brideglabz/addressbooksystem/AddressBook.java
+++ src/com/brideglabz/addressbooksystem/AddressBook.java
@@ -1,4 +1,40 @@
 package com.brideglabz.addressbooksystem;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 public class AddressBook {
+    List<Contact> list = new LinkedList<>();
+    static Scanner sc = new Scanner(System.in);
+    public void addContact() {
+        System.out.println("Enter your firstName : ");
+        String firstName = sc.next();
+        System.out.println("Enter your lastName : ");
+        String lastName = sc.next();
+        System.out.println("Enter your address : ");
+        String address = sc.next();
+        System.out.println("Enter your city : ");
+        String city = sc.next();
+        System.out.println("Enter your state : ");
+        String state = sc.next();
+        System.out.println("Enter your zipCode : ");
+        String zip = sc.next();
+        System.out.println("Enter your phoneNo : ");
+        String phoneNo = sc.next();
+        System.out.println("Enter your emailId : ");
+        String email = sc.next();
+        Contact contact = new Contact();
+        contact.setFirstName(firstName);
+        contact.setEmail(email);
+        contact.setAddress(address);
+        contact.setCity(city);
+        contact.setLastName(lastName);
+        contact.setState(state);
+        contact.setZip(zip);
+        contact.setPhoneNo(phoneNo);
+        list.add(contact);
+    }
+    public void printAddressBook(){
+        System.out.println(list);
+    }
 }
--- src/com/brideglabz/addressbooksystem/AddressBookMain.java
+++ src/com/brideglabz/addressbooksystem/AddressBookMain.java
@@ -2,6 +2,18 @@ package com.brideglabz.addressbooksystem;
 
 public class AddressBookMain {
     public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
         System.out.println("--------------------------------Welcome to the AddressBookSystem program-------------------------------------");
+        addressBook.addContact();
+        addressBook.printAddressBook();
     }
 }
+/*Ability to create a Contacts in Address
+Book with first and last names, address,
+city, state, zip, phone number and
+email...
+- Program is written using IDE like IntelliJ
+- Every UC is in a separate Git Branch and then merged with main
+- Naming Convention, Indentation, etc Code Hygiene will be checked during
+Review
+- Git Check In Comments and Version History will be monitored*/
