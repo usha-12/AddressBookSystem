38768e0a73f994b91117802b50d507b47ebe42e4
usha-12 <usha12ahirwar@gmail.com>
2022-11-13 04:42:09 +0530

    Ability to search person in a city or state acress the multiple addbook

--- src/com/brideglabz/addressbooksystem/AddressBook.java
+++ src/com/brideglabz/addressbooksystem/AddressBook.java
@@ -88,3 +88,12 @@ public class AddressBook {
                 '}';
     }
 }
+/*Ability to search Person
+in a City or State across
+the multiple
+AddressBook - Search Result can show multiple person in
+the city or state
+
+UC 8
+
+- Use Java Streams*/
--- src/com/brideglabz/addressbooksystem/AddressBookMain.java
+++ src/com/brideglabz/addressbooksystem/AddressBookMain.java
@@ -7,7 +7,6 @@ public class AddressBookMain {
     public static void addContact() {
         System.out.println("Enter your firstName : ");
         String firstName = sc.nextLine();
-        // Restricted to duplicate entry
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).getFirstName().equalsIgnoreCase(firstName)) {
                 System.out.println("This name is already exists try with another name");
@@ -16,19 +15,19 @@ public class AddressBookMain {
             }
         }
         System.out.println("Enter your lastName : ");
-        String lastName = sc.next();
+        String lastName = sc.nextLine();
         System.out.println("Enter your address : ");
-        String address = sc.next();
+        String address = sc.nextLine();
         System.out.println("Enter your city : ");
-        String city = sc.next();
+        String city = sc.nextLine();
         System.out.println("Enter your state : ");
-        String state = sc.next();
+        String state = sc.nextLine();
         System.out.println("Enter your zipCode : ");
-        String zip = sc.next();
+        String zip = sc.nextLine();
         System.out.println("Enter your phoneNo : ");
-        String phoneNo = sc.next();
+        String phoneNo = sc.nextLine();
         System.out.println("Enter your emailId : ");
-        String email = sc.next();
+        String email = sc.nextLine();
         AddressBook addressBook = new AddressBook();
         addressBook.setFirstName(firstName);
         addressBook.setEmail(email);
@@ -40,7 +39,6 @@ public class AddressBookMain {
         addressBook.setPhoneNo(phoneNo);
         list.add(addressBook);
     }
-
     public static void addPerson() {
         System.out.println("-----------add multiple person to Address Book-----------------");
         Scanner sc = new Scanner(System.in);
@@ -52,18 +50,31 @@ public class AddressBookMain {
             person++;
         }
     }
-
+    public void searchByCity() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter city name");
+        String city = sc.nextLine();
+        sc.close();
+        for (AddressBook list : list) {
+            if (list.getCity().equals(city))
+                System.out.println(list);
+        }
+    }
     public static void main(String[] args) {
         AddressBookMain addressBookMain = new AddressBookMain();
         System.out.println("Welcome to Address Book Program..");
         addPerson();
+       addressBookMain.searchByCity();
         System.out.println(list);
 
     }
 }
-/*Ability to ensure there is no Duplicate
-Entry of the same Person in a particular
-Address Book - Duplicate Check is done on Person Name while adding
-person to Address Book.
-- Use Collection Methods to Search Person by Name for
-Duplicate Entry*/
+/*Ability to search Person
+in a City or State across
+the multiple
+AddressBook - Search Result can show multiple person in
+the city or state
+
+UC 8
+
+- Use Java Streams*/
