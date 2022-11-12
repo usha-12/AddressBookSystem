e7adb853549ae7c287e654e101b984dcab7c5671
usha-12 <usha12ahirwar@gmail.com>
2022-11-13 04:12:28 +0530

    Refactor to add multiple address book to the system has a unique name

--- src/com/brideglabz/addressbooksystem/AddressBookMain.java
+++ src/com/brideglabz/addressbooksystem/AddressBookMain.java
@@ -1,12 +1,24 @@
 package com.brideglabz.addressbooksystem;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class AddressBookMain {
-   static Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
     static List<AddressBook> list = new ArrayList<AddressBook>();
+    static List<AddressBookRecords> bookNameLst = new ArrayList<>();
+    static Map<Integer, String> booksLst = new HashMap<>();
+    boolean result;
+
     public static void addContact() {
+        System.out.println("Enter your firstName : ");
+        String firstName1 = sc.nextLine();
+        // Restricted to duplicate entry
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getFirstName().equalsIgnoreCase(firstName1)) {
+                System.out.println("This name is already exists try with another name");
+                addMultiplePerson();
+                break;
+            }
+        }
         System.out.println("Enter your firstName : ");
         String firstName = sc.next();
         System.out.println("Enter your lastName : ");
@@ -34,6 +46,7 @@ public class AddressBookMain {
         addressBook.setPhoneNo(phoneNo);
         list.add(addressBook);
     }
+
     public static void editContact() {
         System.out.println("------------edit existing contact person using their name--------");
         Scanner sc = new Scanner(System.in);
@@ -49,6 +62,7 @@ public class AddressBookMain {
         }
         sc.close();
     }
+
     public static void deleteContact() {
         System.out.println("------------------------Delete a person using person's name------------------------------------");
         Scanner sc = new Scanner(System.in);
@@ -63,6 +77,7 @@ public class AddressBookMain {
         }
         sc.close();
     }
+
     public static void addMultiplePerson() {
         System.out.println("-----------add multiple person to Address Book-----------------");
         Scanner sc = new Scanner(System.in);
@@ -75,16 +90,45 @@ public class AddressBookMain {
         }
     }
 
+    public void addAddressBook() {
+        System.out.println("Enter id : ");
+        int id = sc.nextInt();
+        System.out.println("Enter name : ");
+        String bookName = sc.next();
+        AddressBookRecords book = new AddressBookRecords(id, bookName);
+
+        if (bookNameLst.isEmpty()) {
+            bookNameLst.add(book);
+            booksLst.put(id, bookName);
+            System.out.println("New record added  : ");
+        } else {
+            for (int i = 0; i < bookNameLst.size(); i++) {
+                if (bookNameLst.get(i).getBookName().equals(bookName)) {
+                    result = bookNameLst.get(i).getBookName().equals(bookName);
+                    System.out.println("Record is already exists : ");
+                    break;
+                }
+            }
+            if (!result) {
+                bookNameLst.add(book);
+                booksLst.put(id, bookName);
+                System.out.println("Record added successfully : ");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         AddressBookMain addressBookMain = new AddressBookMain();
         System.out.println("Welcome to Address Book Program..");
+        addressBookMain.addAddressBook();
         addContact();
-        addMultiplePerson();
+        System.out.println(bookNameLst);
         System.out.println(list);
     }
 }
-/*Ability to add multiple
-person to Address Book
-- Use Console to add person details one at a time
-- Use Collection Class to maintain multiple
-contact persons in Address Book*/
+/*Refactor to add multiple
+Address Book to the
+System. Each Address Book
+has a unique Name - Use Console to add new Address Book - Maintain Dictionary of Address Book Name to
+
+Address*/
